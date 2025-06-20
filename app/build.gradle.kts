66269d761468feda6279a071ae098a418ad5ac1e
Damir Samigullin <d.f.samigullin@mail.ru>
2025-06-20 14:03:21 +0300

    add gradle-project

---
+++ app/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java")
+    id("com.github.ben-manes.versions") version "0.52.0"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}

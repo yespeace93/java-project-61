79250c51f736b2dd1211342b6038bd85714243c5
Damir Samigullin <d.f.samigullin@mail.ru>
2025-06-30 13:18:13 +0300

    update App.java using Scanner

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,28 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 class App {
     public static void main(String[] args) {
-        Cli.greetUser();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
+
+        int numberOfGame = scanner.nextInt();
+
+        System.out.println("Your choice: " + numberOfGame);
+
+        switch (numberOfGame) {
+            case 0:
+                scanner.close();
+                break;
+            case 1:
+                hexlet.code.Cli.greetUser();
+                break;
+            default:
+                System.out.println("Error! Choose a number from 0 to 1.");
+        }
     }
 }
